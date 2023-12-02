9d7cafdb3eeafb5329f83161a39db8d41f58c9fe
Bass913 <bastiendikiadi@outlook.fr>
2023-12-02 12:18:21 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="hero min-h-screen bg-contain"
+    style="background-image: url(/img/chef-retard.webp)"
+  >
+    <div class="hero-overlay bg-opacity-60"></div>
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-lg">
+        <h1 class="mb-5 text-5xl font-bold">
+          Bievenue sur les "Chroniques du Chef Retard"
+        </h1>
+        <p class="mb-5">
+          Vous avez déjà entendu parler d'un chef qui court toujours après le
+          temps ? Ou qui arrive systématiquement en retard, ajoutant une touche
+          d'inattendu à chaque journée ? Bienvenue dans le monde captivant et
+          hilarant des "Chroniques du Chef Retard" ! Notre site web est votre
+          fenêtre vers les anecdotes, les péripéties et les moments de comédie
+          du quotidien de ce chef pas comme les autres.
+        </p>
+        <button class="btn btn-primary">Plongez dans son univers !</button>
+      </div>
+    </div>
+  </div>
+</template>
