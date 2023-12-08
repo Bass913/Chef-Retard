fe46cf2c955fbc06c2be2bebd0166ef49e9d0172
Bass913 <bastiendikiadi@outlook.fr>
2023-12-08 16:52:08 +0100

    meta description and title added

--- app.vue
+++ app.vue
@@ -1,20 +1,16 @@
 <script setup lang="ts">
-useHead({
+useSeoMeta({
   title: "Chef Retard : Cuisinez avec Saveur, Même en Mode Express !",
-
-  meta: [
-    {
-      name: "description",
-      content:
-        "Découvrez l'univers délicieux de Chef Retard, où la cuisine rapide et savoureuse devient un art. Trouvez des recettes faciles pour les chefs pressés, des astuces de préparation et des idées créatives pour des repas délicieux en un temps record. Relevez le défi culinaire du quotidien avec nos solutions gourmandes, spécialement conçues pour ceux qui ont peu de temps mais beaucoup de goût !",
-    },
-  ],
-  bodyAttrs: {
-    class: "test",
-  },
-  script: [{ innerHTML: "console.log('Hello world')" }],
+  ogTitle: "Chef Retard : Cuisinez avec Saveur, Même en Mode Express !",
+  description:
+    "Découvrez l'univers délicieux de Chef Retard, où la cuisine rapide et savoureuse devient un art. Trouvez des recettes faciles pour les chefs pressés, des astuces de préparation et des idées créatives pour des repas délicieux en un temps record. Relevez le défi culinaire du quotidien avec nos solutions gourmandes, spécialement conçues pour ceux qui ont peu de temps mais beaucoup de goût !",
+  ogDescription:
+    "Découvrez l'univers délicieux de Chef Retard, où la cuisine rapide et savoureuse devient un art. Trouvez des recettes faciles pour les chefs pressés, des astuces de préparation et des idées créatives pour des repas délicieux en un temps record. Relevez le défi culinaire du quotidien avec nos solutions gourmandes, spécialement conçues pour ceux qui ont peu de temps mais beaucoup de goût !.",
+  ogImage: "https://example.com/image.png",
+  twitterCard: "summary_large_image",
 });
 </script>
+
 <template>
   <div>
     <NuxtLayout>
