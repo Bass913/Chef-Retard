e61a5bad6e60878a4b98129a250cae43f62f0973
Bass913 <bastiendikiadi@outlook.fr>
2023-12-08 16:44:30 +0100

    site completely changed

---
+++ components/AppTestimonial.vue
@@ -0,0 +1,164 @@
+<template>
+  <section class="my-8 dark:bg-gray-800 dark:text-gray-100">
+    <div
+      class="container flex flex-col items-center mx-auto mb-12 md:p-10 md:px-12"
+    >
+      <h2 class="p-4 text-4xl font-semibold leadi text-center">
+        Ce que nos visiteurs disent de nous
+      </h2>
+    </div>
+    <div
+      class="container flex flex-col items-center justify-center mx-auto lg:flex-row lg:flex-wrap lg:justify-evenly lg:px-10"
+    >
+      <div class="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+        <div class="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+          <p
+            class="relative px-6 py-1 text-lg italic text-center dark:text-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              class="w-8 h-8 dark:text-violet-400"
+            >
+              <path
+                d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"
+              ></path>
+              <path
+                d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"
+              ></path></svg
+            >Chef Retard a été une véritable révélation pour moi en tant que
+            maman débordée. Grâce à leurs recettes rapides et délicieuses, je
+            peux maintenant préparer des repas savoureux pour ma famille sans
+            sacrifier des heures de ma journée. C'est devenu mon allié
+            incontournable pour des dîners rapides et réussis !
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              class="absolute right-0 w-8 h-8 dark:text-violet-400"
+            >
+              <path
+                d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"
+              ></path>
+              <path
+                d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"
+              ></path>
+            </svg>
+          </p>
+        </div>
+        <div
+          class="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900"
+        >
+          <img
+            src="https://source.unsplash.com/50x50/?portrait?1"
+            alt=""
+            class="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+          />
+          <p class="text-xl font-semibold leadi">Sophie, Maman débordée</p>
+        </div>
+      </div>
+      <div class="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+        <div class="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+          <p
+            class="relative px-6 py-1 text-lg italic text-center dark:text-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              class="w-8 h-8 dark:text-violet-400"
+            >
+              <path
+                d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"
+              ></path>
+              <path
+                d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"
+              ></path></svg
+            >En tant qu'étudiant avec un emploi du temps chargé, Chef Retard a
+            révolutionné ma façon de cuisiner. Les recettes simples et les
+            astuces pratiques m'ont permis de concocter des repas équilibrés en
+            un temps record. Fini le fast-food, bonjour la cuisine rapide et
+            délicieuse !
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              class="absolute right-0 w-8 h-8 dark:text-violet-400"
+            >
+              <path
+                d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"
+              ></path>
+              <path
+                d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"
+              ></path>
+            </svg>
+          </p>
+        </div>
+        <div
+          class="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900"
+        >
+          <img
+            src="https://source.unsplash.com/50x50/?portrait?2"
+            alt=""
+            class="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+          />
+          <p class="text-xl font-semibold leadi">Nicolas, Etudiant pressé</p>
+          <p class="text-sm uppercase"></p>
+        </div>
+      </div>
+      <div class="flex flex-col max-w-sm mx-4 my-6 shadow-lg">
+        <div class="px-4 py-12 rounded-t-lg sm:px-8 md:px-12 dark:bg-gray-900">
+          <p
+            class="relative px-6 py-1 text-lg italic text-center dark:text-gray-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              class="w-8 h-8 dark:text-violet-400"
+            >
+              <path
+                d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"
+              ></path>
+              <path
+                d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"
+              ></path></svg
+            >Chef Retard a transformé mes soirées après le travail. En tant que
+            professionnelle active, je n'ai pas toujours le temps de passer des
+            heures en cuisine. Les recettes astucieuses de Chef Retard m'ont
+            sauvé la vie, offrant des plats savoureux et faits maison sans
+            sacrifier ma précieuse soirée. Une vraie bouffée d'air frais pour
+            les chefs pressés comme moi !
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              fill="currentColor"
+              class="absolute right-0 w-8 h-8 dark:text-violet-400"
+            >
+              <path
+                d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"
+              ></path>
+              <path
+                d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"
+              ></path>
+            </svg>
+          </p>
+        </div>
+
+        <div
+          class="flex flex-col items-center justify-center p-8 rounded-b-lg dark:bg-violet-400 dark:text-gray-900"
+        >
+          <img
+            src="https://source.unsplash.com/50x50/?portrait?4"
+            alt=""
+            class="w-16 h-16 mb-2 -mt-16 bg-center bg-cover rounded-full dark:bg-gray-500 dark:bg-gray-700"
+          />
+          <p class="text-xl font-semibold leadi">
+            Marie, Professionnelle active
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
